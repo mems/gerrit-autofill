7580734a71a8ac64bf09fc4cc597e3a7aa9db951
Memmie Lenglet <memmie@lenglet.name>
2014-07-24 11:59:07 +0200

    Create a README

---
+++ README.md
@@ -0,0 +1,7 @@
+## Gerrit autofill
+
+In GWT forms are submitted with AJAX, but browser's form autofill not work on that forms because they are not real one (no form tag)
+
+- http://stackoverflow.com/questions/4841156/gwt-and-autofill
+- http://jamies-gwt.blogspot.fr/2011/08/gwt-and-usernamepassword-autocomplete.html
+- http://wayback.archive.org/web/20130201020841/http://borglin.net/gwt-project/?page_id=467
--- gerrit_autofill.user.js
+++ gerrit_autofill.user.js
@@ -7,12 +7,6 @@
 // @grant	    none
 // ==/UserScript==
 
-// In GWT forms are submitted with AJAX
-// But browser's form autofill not work on that forms because they are not real one (no form tag)
-// http://stackoverflow.com/questions/4841156/gwt-and-autofill
-// http://jamies-gwt.blogspot.fr/2011/08/gwt-and-usernamepassword-autocomplete.html
-// http://wayback.archive.org/web/20130201020841/http://borglin.net/gwt-project/?page_id=467
-
 /**
 * Create mutation observer will observe nodes addition
 */
